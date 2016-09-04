11459e0a58431c7064f25f86bed1069cbed31614
frontend-coder <verstka.shablonov@gmail.com>
2016-09-04 23:18:59 +0300

    0.21
    нет адаптивности, не проверена ФОС

--- js/common.js
+++ js/common.js
@@ -19,6 +19,33 @@ return false;
 	});
 
 
+	$(".top-menu ul li a").mPageScroll2id({
+		 layout:"auto",
+		 offset:".top-line",
+		scrollEasing: "linear",
+		highlightByNextTarget: true,
+		keepHighlightUntilNext: true,
+		 autoScrollSpeed: true,
+		scrollSpeed : 1000
+	});
+
+
+
+//Кнопка наверх с права от контента
+$("body").append('<div class="top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></div>');
+// Заставляет кнопку работать как ссылку на самый вверх
+$("body").on("click", ".top", function() {
+	$("html, body").animate({scrollTop: 0}, "slow");
+});
+// Заставляет прятаться кнопку, если посетитель на хедере
+$(window).scroll(function() {
+if ($(this).scrollTop() > $(this).height()) {
+	$(".top").addClass("active");
+} else 
+{  	$(".top").removeClass("active");
+}
+});
+	
 // галерея всплывающая
 $(".portfolio-item").each(function(e){
 var th = $(this);
@@ -34,82 +61,6 @@ type:'inline',
 
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	//Таймер обратного отсчета
-	//Документация: http://keith-wood.name/countdown.html
-	//<div class="countdown" date-time="2015-01-07"></div>
-	var austDay = new Date($(".countdown").attr("date-time"));
-	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
-
-	//Попап менеджер FancyBox
-	//Документация: http://fancybox.net/howto
-	//<a class="fancybox"><img src="image.jpg" /></a>
-	//<a class="fancybox" data-fancybox-group="group"><img src="image.jpg" /></a>
-	$(".fancybox").fancybox();
-
-	//Навигация по Landing Page
-	//$(".top_mnu") - это верхняя панель со ссылками.
-	//Ссылки вида <a href="#contacts">Контакты</a>
-	$(".top_mnu").navigation();
-
-	//Добавляет классы дочерним блокам .block для анимации
-	//Документация: http://imakewebthings.com/jquery-waypoints/
-	$(".block").waypoint(function(direction) {
-		if (direction === "down") {
-			$(".class").addClass("active");
-		} else if (direction === "up") {
-			$(".class").removeClass("deactive");
-		};
-	}, {offset: 100});
-
-	//Плавный скролл до блока .div по клику на .scroll
-	//Документация: https://github.com/flesler/jquery.scrollTo
-	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
-			offset: -90
-		});
-	});
-
-	//Каруселька
-	//Документация: http://owlgraphic.com/owlcarousel/
-	var owl = $(".carousel");
-	owl.owlCarousel({
-		items : 4
-	});
-	owl.on("mousewheel", ".owl-wrapper", function (e) {
-		if (e.deltaY > 0) {
-			owl.trigger("owl.prev");
-		} else {
-			owl.trigger("owl.next");
-		}
-		e.preventDefault();
-	});
-	$(".next_button").click(function(){
-		owl.trigger("owl.next");
-	});
-	$(".prev_button").click(function(){
-		owl.trigger("owl.prev");
-	});
-
-	//Кнопка "Наверх"
-	//Документация:
-	//http://api.jquery.com/scrolltop/
-	//http://api.jquery.com/animate/
 	$("#top").click(function () {
 		$("body, html").animate({
 			scrollTop: 0
